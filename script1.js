8add8b923764e7d34348444e1561565cbbdc383f
its om <bheesammahasis108@gmail.com>
2023-03-30 13:12:00 +0530

    OM12

--- script1.js
+++ script1.js
@@ -5,20 +5,19 @@ setInterval(showTime, 1000);
         	let min = time.getMinutes();
         	let sec = time.getSeconds();
         	let am_pm = "PM";
-        
-        	if (hour > 12) {
-        		hour -= 12;
-        		am_pm = "PM";
-        	}
-        	if (hour == 0) {
-        		hr = 12;
-        		am_pm = "AM";
-        	}
 			if (hour < 12) {
         		am_pm = "AM";
         	}
-        
-        	hour = hour < 10 ? "0" + hour : hour;
+		    if (hour > 12) {
+        		hour -= 12;
+        		am_pm = "PM";
+        	}
+			if (hour == 0) {
+        		hr = 12;
+        		am_pm = "AM";
+        	}
+
+            hour = hour < 10 ? "0" + hour : hour;
         	min = min < 10 ? "0" + min : min;
         	sec = sec < 10 ? "0" + sec : sec;
         

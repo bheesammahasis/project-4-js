f37018280308190c77a99b3d69d2048b401a1190
its om <bheesammahasis108@gmail.com>
2023-03-30 11:11:47 +0530

    omup

--- script1.js
+++ script1.js
@@ -28,55 +28,58 @@ setInterval(showTime, 1000);
         showTime();
 
 		//set wakeup time (5am to 12pm) image set,text set,etc
-        let textchange1=document.querySelector(".item4")
+        let textchange1=document.getElementById("i8")
 		textchange1.addEventListener('click',()=>{
 			let time=new Date();
 			hour=time.getHours();
+			console.log(hour)
         if(hour>=5 && hour<12){
-           textchange1.innerHTML="GRAB SOME HEALTHY BREAKFAST!!";
+			document.getElementById("i4").innerText="GRAB SOME HEALTHY BREAKFAST!!";
         }
         else if(hour>=12 && hour<5){
-           textchange1.innerHTML="LET'S HAVE SOME LUNCH!!";
+			document.getElementById("i4").innerText="LET'S HAVE SOME LUNCH!!";
         }
         if(hour>=5 && hour<9){
-           textchange1.innerHTML="STOP YAWNING, GET SOME TEA...ITS JUST EVENING TIME";
+			document.getElementById("i4").innerText="STOP YAWNING, GET SOME TEA...ITS JUST EVENING TIME";
         }
         if(hour>=9 && hour<5){
-            textchange1.innerHTML="CLOSE YOUR EYES AND GOTO SLEEP";
+			document.getElementById("i4").innerTextL="CLOSE YOUR EYES AND GOTO SLEEP";
         }
 	});
-
-		let textchange2 = document.querySelector(".item8");
-		textchange2.addEventListener('click',()=>{
+	
+        function myom1(){
 			let time=new Date();
 			hour=time.getHours();
 			let time1a = document.getElementById("time1").value ;
 			let time1b = document.getElementById("time2").value ;
 			let time1c = document.getElementById("time3").value ;
 			let time1d = document.getElementById("time4").value ;
-			let image = document.querySelector('');
+			let image = document.querySelector('.i2');
 			console.log(hour,time1a,time1b,time1c,time1d);
 			if(hour===parseInt(time1a)){
-				item7.innerHTML="GOOD MORNING!! WAKE UP!!";
-				Image.src="Group 5183.svg";
-				item9.innerHTML="Wake Up Time:5AM-8AM <br>Lunch Time:default<br>Nap Time:default<br>Night Time:default";
+				console.log(hour,time1a);
+				document.getElementById("i7").innerText="GOOD MORNING!! WAKE UP!!";
+			document.getElementById("imgi5").src="Group 5183.svg";
+			document.getElementById("i9").innerText="Wake Up Time:5AM-8AM <br>Lunch Time:default<br>Nap Time:default<br>Night Time:default";
 			}
-			if(hour===parseInt(time1b)){
-				item7.innerHTML="GOOD AFTERNOON!! TAKE SOME SLEEP";
-				Image.src="Group 5183 B.svg";
-				item9.innerHTML="Wake Up Time:default<br>Lunch Time:12PM-5PM<br>Nap Time:default<br>Night Time:default";
+			else if(hour===parseInt(time1b)){
+				console.log(hour,time1b);
+				document.getElementById("i7").innerText="GOOD AFTERNOON!! TAKE SOME SLEEP";
+				document.getElementById("imgi5").src="Group 5183 B.svg";
+				document.getElementById("i9").innerText="Wake Up Time:default<br>Lunch Time:12PM-5PM<br>Nap Time:default<br>Night Time:default";
 			}
-			if(hour===parseInt(time1c)){
-				item7.innerHTML="GOOD EVENING!!";
-				Image.src="Glunch_image.png";
-				item9.innerHTML="Wake Up Time:default <br>Lunch Time:default<br>Nap Time:5PM-9PM<br>Night Time:default";
+			else if(hour===parseInt(time1c)){
+				console.log(hour,time1c);
+				document.getElementById("i7").innerText="GOOD EVENING!!";
+				document.getElementById("imgi5").src="Glunch_image.png";
+				document.getElementById("i9").innerText="Wake Up Time:default <br>Lunch Time:default<br>Nap Time:5PM-9PM<br>Night Time:default";
 			}
-			if(hour===parseInt(time1d)){
-				item7.innerHTML="GOOD NIGHT!!";
-				Image.src="Group 5194.svg";
-				item9.innerHTML="Wake Up Time:default <br>Lunch Time:default<br>Nap Time:default<br>Night Time:9PM-5AM";
+			else if(hour===parseInt(time1d)){
+				console.log(hour,time1d);
+				document.getElementById("i7").innerText="GOOD NIGHT!!";
+				document.getElementById("imgi5").src="Group 5194.svg";
+				document.getElementById("i9").innerText="Wake Up Time:default <br>Lunch Time:default<br>Nap Time:default<br>Night Time:9PM-5AM";
 			}
-	});
-        
+}
         
 	

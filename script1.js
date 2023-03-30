473b230ca59f783c9de13632b73519773f1b1f6b
its om <bheesammahasis108@gmail.com>
2023-03-30 13:06:07 +0530

    OM1

--- script1.js
+++ script1.js
@@ -4,7 +4,7 @@ setInterval(showTime, 1000);
         	let hour = time.getHours();
         	let min = time.getMinutes();
         	let sec = time.getSeconds();
-        	let am_pm = "AM";
+        	let am_pm = "PM";
         
         	if (hour > 12) {
         		hour -= 12;
@@ -28,7 +28,47 @@ setInterval(showTime, 1000);
         showTime();
 
 		//set wakeup time (5am to 12pm) image set,text set,etc
-        let textchange1=document.getElementById("i8")
+       
+        function myom1(){
+			let time=new Date();
+			hour=time.getHours();
+			let time1a = document.getElementById("time1").value ;
+			let time1b = document.getElementById("time2").value ;
+			let time1c = document.getElementById("time3").value ;
+			let time1d = document.getElementById("time4").value ;
+			//let image = document.querySelector('#imgi5');
+			console.log(hour,time1a,time1b,time1c,time1d);
+			if(hour===parseInt(time1a)){
+				console.log(hour,time1a);
+				document.getElementById("i7").innerText="GOOD MORNING!! WAKE UP!!";
+			document.getElementById("imgi5").src="Group 5183.svg";
+			document.getElementById("i9").innerText="Wake Up Time:5AM-8AM;Lunch Time:default;Nap Time:default;Night Time:default";
+			document.getElementById("i4").innerText="GRAB SOME HEALTHY BREAKFAST!!";
+			}
+			else if(hour===parseInt(time1b)){
+				console.log(hour,time1b);
+				document.getElementById("i7").innerText="GOOD AFTERNOON!! TAKE SOME SLEEP";
+				document.getElementById("imgi5").src="Group 5183 B.svg";
+				document.getElementById("i9").innerText="Wake Up Time:default;Lunch Time:12PM-5PM;Nap Time:default;Night Time:default";
+				document.getElementById("i4").innerText="LET'S HAVE SOME LUNCH!!";
+			}
+			else if(hour===parseInt(time1c)){
+				console.log(hour,time1c);
+				document.getElementById("i7").innerText="GOOD EVENING!!";
+				document.getElementById("imgi5").src="Glunch_image.png";
+				document.getElementById("i9").innerText="Wake Up Time:default ;Lunch Time:default;Nap Time:5PM-9PM;Night Time:default";
+				document.getElementById("i4").innerText="STOP YAWNING, GET SOME TEA...ITS JUST EVENING TIME";
+			}
+			else if(hour===parseInt(time1d)){
+				console.log(hour,time1d);
+				document.getElementById("i7").innerText="GOOD NIGHT!!";
+				document.getElementById("imgi5").src="Group 5194.svg";
+				document.getElementById("i9").innerText="Wake Up Time:default ;Lunch Time:default;Nap Time:default;Night Time:9PM-5AM";
+				document.getElementById("i4").innerTextL="CLOSE YOUR EYES AND GOTO SLEEP";
+			}
+}
+        
+	/* let textchange1=document.getElementById("i8")
 		textchange1.addEventListener('click',()=>{
 			let time=new Date();
 			hour=time.getHours();
@@ -45,41 +85,5 @@ setInterval(showTime, 1000);
         if(hour>=9 && hour<5){
 			document.getElementById("i4").innerTextL="CLOSE YOUR EYES AND GOTO SLEEP";
         }
-	});
-	
-        function myom1(){
-			let time=new Date();
-			hour=time.getHours();
-			let time1a = document.getElementById("time1").value ;
-			let time1b = document.getElementById("time2").value ;
-			let time1c = document.getElementById("time3").value ;
-			let time1d = document.getElementById("time4").value ;
-			//let image = document.querySelector('.i2');
-			console.log(hour,time1a,time1b,time1c,time1d);
-			if(hour===parseInt(time1a)){
-				console.log(hour,time1a);
-				document.getElementById("i7").innerText="GOOD MORNING!! WAKE UP!!";
-			document.getElementById("imgi5").src="Group 5183.svg";
-			document.getElementById("i9").innerText="Wake Up Time:5AM-8AM <br>Lunch Time:default<br>Nap Time:default<br>Night Time:default";
-			}
-			else if(hour===parseInt(time1b)){
-				console.log(hour,time1b);
-				document.getElementById("i7").innerText="GOOD AFTERNOON!! TAKE SOME SLEEP";
-				document.getElementById("imgi5").src="Group 5183 B.svg";
-				document.getElementById("i9").innerText="Wake Up Time:default<br>Lunch Time:12PM-5PM<br>Nap Time:default<br>Night Time:default";
-			}
-			else if(hour===parseInt(time1c)){
-				console.log(hour,time1c);
-				document.getElementById("i7").innerText="GOOD EVENING!!";
-				document.getElementById("imgi5").src="Glunch_image.png";
-				document.getElementById("i9").innerText="Wake Up Time:default <br>Lunch Time:default<br>Nap Time:5PM-9PM<br>Night Time:default";
-			}
-			else if(hour===parseInt(time1d)){
-				console.log(hour,time1d);
-				document.getElementById("i7").innerText="GOOD NIGHT!!";
-				document.getElementById("imgi5").src="Group 5194.svg";
-				document.getElementById("i9").innerText="Wake Up Time:default <br>Lunch Time:default<br>Nap Time:default<br>Night Time:9PM-5AM";
-			}
-}
-        
+	});*/
 	
